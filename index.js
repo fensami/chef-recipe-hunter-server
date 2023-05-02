bd396f1253448865731791a44b912835856eb2cc
fensami <113832498+fensami@users.noreply.github.com>
2023-05-02 16:56:55 +0600

    editing some data chefdata file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const foodian = require('./data/foodian.json')
+const chefdata = require('./data/chefdata.json')
+
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -9,14 +12,24 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send('Foodian is running...')
 })
+app.get('/foodian', (req, res) => {
+    res.send(foodian)
+})
+app.get('/chefdata', (req, res) => {
+    res.send(chefdata)
+})
+app.get('/chefdata/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedData = chefdata.find(d=> d.id === id);
+    res.send(selectedData)
+})
 
 app.listen(port, () => {
     console.log(`foodian api is running on port: ${port}`);
 })
 
-// app.get('/', (req, res) => {
-//     res.send("Dragon is running...")
-// })
+
 // app.get('/categories', (req, res) => {
 //     res.send(categories)
 // })
